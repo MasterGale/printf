8e42e6d628d46306b142ff6b1b5dfc6019e9c398
Hezron-Simumba <simumbahezron@gmail.com>
2023-06-29 12:51:00 -0700

    main.h

--- main.h
+++ main.h
@@ -3,7 +3,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
@@ -37,7 +36,7 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
-typedef struct fmt fmt_t
+typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
